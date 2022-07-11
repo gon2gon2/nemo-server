a409526767b19f37ab18ba342a31f67e1b8fde89
gon2gon2 <ark5354@gmail.com>
2022-07-11 16:09:34 +0900

    eslint commonjs 제거

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
     node: true,
     jest: true
   },
+
   extends: ['airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
