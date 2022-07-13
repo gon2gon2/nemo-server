a7433ba5752d76fc806444b7a7511df0094add7a
gon2gon2 <ark5354@gmail.com>
2022-07-13 13:57:14 +0900

    #13 feat: 명함첩 페이지를 위한 api

--- controllers/tag.controller.js
+++ controllers/tag.controller.js
@@ -23,4 +23,13 @@ Controller.findWithName = (tag_name) => {
   return founded_tag;
 }
 
+// 태그아이디 -> 태그명
+Controller.findWithId = (tag_id) => {
+  const founded_tag = Tag.findOne({where: {"id": tag_id}}) 
+  if (!founded_tag){
+    return false
+  }
+  return founded_tag;
+}
+
 export default Controller;
