e5065c18b0ce109ee62a6a2f7df51fdca37de0ae
gon2gon2 <ark5354@gmail.com>
2022-07-13 17:35:24 +0900

    #18 feat: 내 명함 조회 API 추가

--- controllers/card.controller.js
+++ controllers/card.controller.js
@@ -4,13 +4,22 @@ const {Card} = db;
 const Controller = {};
 
 // Create and Save a new user
-Controller.create = (data) => {
+Controller.create = async (data) => {
   const {user_id, nickname, tag_id_1, tag_id_2, tag_id_3, intro, img_url} = data;
-  const new_card = Card.create({user_id, nickname, tag_id_1, tag_id_2, tag_id_3, intro, img_url});
+  const new_card = await Card.create({user_id, nickname, tag_id_1, tag_id_2, tag_id_3, intro, img_url});
   if (!new_card){
     return false;
   }
   return true;
 };
+
+
+Controller.findWithUserId = async (user_id) => {
+  const foundedCard = await Card.findOne({where: {user_id}});
+  if (!foundedCard){
+    return false;
+  }
+  return foundedCard;
+}
 
 export default Controller;
