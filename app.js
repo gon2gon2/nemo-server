314f54089947c2d40a6707fd8251bd48dc55a32b
gon2gon2 <ark5354@gmail.com>
2022-07-13 11:05:49 +0900

    #15 fix: user password no field 수정

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ import userService from './services/user.service.js';
 import cardService from './services/card.service.js';
 import profileService from './services/profile.service.js';
 import tempService from './services/temp.service.js'
+// database
+/* test시 오류가 나고, sync를 하지 않아도 db에서 데이터는 잘 가져옴. 
+  테이블 구조 바뀌었을 때만 실행해주면 되는 것 같다.
+  */
+
+import db from './models/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -46,19 +52,13 @@ app.use((err, req, res) => {
   res.status(err.status || 500);
   res.render('error');
 });
-// database
-/* test시 오류가 나고, sync를 하지 않아도 db에서 데이터는 잘 가져옴. 
-  테이블 구조 바뀌었을 때만 실행해주면 되는 것 같다.
-  */
-
-// import db from './models/index.js';
-// db.sequelize
-//   .sync()
-//   .then(() => {
-//     console.log('Synced db.');
-//   })
-//   .catch(err => {
-//     console.log(`Failed to sync db: ${  err.message}`);
-//   });
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log('Synced db.');
+  })
+  .catch(err => {
+    console.log(`Failed to sync db: ${  err.message}`);
+  });
 
 export default app;
--- services/user.service.js
+++ services/user.service.js
@@ -20,11 +20,13 @@ export default app => {
   router.post('/login', async (req, res) => {
     const {body} = req;
     const user = await users.findWithAccountName(body.account_name);
-    if (user.password === body.password){
-      const user_id = user.id;
+    const user_data = user.dataValues;
+
+    if (user_data.password === body.password){
+      const user_id = user_data.id;
       res.status(200).send({user_id});
     }
-    else {
+    else { // 계정이 없거나 비번 다른 경우에도 같은 처리
       res.status(404).send("Not found");
     }
   });
