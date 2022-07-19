b11af982a096a4021c136f480eca0c5d59569ceb
gon2gon2 <ark5354@gmail.com>
2022-07-19 12:16:33 +0900

    #27: DB 분리

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@ docker kill api;
 docker system prune -af --volumes;
 docker pull gcr.io/red-seeker-356107/api;
 docker run -p 3000:3000\
-    -v ./uploads:/nemo/uploads
+    -v ./uploads:/nemo/uploads\
     -e NODE_ENV=production\
     --env-file ./config/.env\
     --name=api\
