263a1fd6c3df5979ef00b2c8a9d184c88d3d1e2c
gon2gon2 <ark5354@gmail.com>
2022-07-19 03:33:24 +0000

    #27 DB분리

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,9 @@ docker kill api;
 docker system prune -af --volumes;
 docker pull gcr.io/red-seeker-356107/api;
 docker run -p 3000:3000\
-    -v ./uploads:/nemo/uploads\
+    -v /home/fivenemos/nemo-express/uploads:/nemo/uploads\
     -e NODE_ENV=production\
-    --env-file ./config/.env\
+    --env-file /home/fivenemos/nemo-express/config/.env\
     --name=api\
-    -d gcr.io/red-seeker-356107/api\
+    gcr.io/red-seeker-356107/api\
     ;
