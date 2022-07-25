6080c0987f4e4a87bc2942e9d92d7161420d92f6
gon2gon2 <ark5354@gmail.com>
2022-07-25 10:30:42 +0900

    #24 feat: 명함첩에서 삭제 기능

--- tests/const.js
+++ tests/const.js
@@ -1,5 +1,7 @@
-const TIMELIMIT = 10000;
+const TIMELIMIT = 20000;
 export const USER_ID = 'testID';
 export const USER_PW = 'testPW';
 export const USER_PN = '010-0000-0000';
+export const USER_ID_1 = '123';
+export const USER_ID_2 = '789';
 export default TIMELIMIT;
