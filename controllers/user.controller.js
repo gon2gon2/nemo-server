9e518de0b8ede3842fb05bf7f8761c83c293f890
gon2gon2 <ark5354@gmail.com>
2022-08-27 20:51:26 +0900

    #5 fix: 불필요한 쿼리, 함수 삭제

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,7 @@
 import db from '../models/index.js';
 
-const { User, Card, Tag } = db;
+const { User } = db;
 const Controller = {};
-const { Op } = db.Sequelize;
 
 Controller.create = async data => {
   const { account_name, password, phone_number, salt } = data;
@@ -36,43 +35,6 @@ Controller.findWithAccountName = async account_name => {
     return false;
   }
   return founded_user;
-};
-
-Controller.findFriends = async account_id => {
-  const founded_user = await User.findOne({ where: { id: account_id } });
-  const friends = founded_user.friends.split(',');
-  const allCards = await Card.findAll({
-    where: {
-      user_id: { [Op.in]: friends },
-    },
-  });
-
-  const result = [];
-  for (let i = 0; i < allCards.length; i += 1) {
-    const { nickname, image, user_id, intro, tag_id_1, tag_id_2, tag_id_3 } =
-      allCards[i];
-    const tagObj_1 = await Tag.findOne({ where: { id: tag_id_1 } });
-    const tagObj_2 = await Tag.findOne({ where: { id: tag_id_2 } });
-    const tagObj_3 = await Tag.findOne({ where: { id: tag_id_3 } });
-
-    const tag_1 = tagObj_1.tag_name;
-    const tag_2 = tagObj_2.tag_name;
-    const tag_3 = tagObj_3.tag_name;
-
-    const converted = {
-      nickname,
-      image,
-      user_id,
-      intro,
-      tag_1,
-      tag_2,
-      tag_3,
-    };
-    result.push(converted);
-  }
-  // founded_user.friends
-  // .freinds로 card에서 하나씩 꺼내온다.
-  return result;
 };
 
 Controller.updateUser = async data => {
